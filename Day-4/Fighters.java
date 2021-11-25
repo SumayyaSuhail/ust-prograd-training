295a9dd8d26d6566d76c9faaa79451330cba3558
Sumayya Suhail <anzam.suhail4321@gmail.com>
2021-11-25 19:11:03 +0530

    Day 4 codes

---
+++ Day-4/Fighters.java
@@ -0,0 +1,20 @@
+public class Fighters {
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        boolean fighter1Turn=false;
+        if(firstAttacker==fighter1.name){
+            fighter1Turn=true;
+        }
+        while (fighter1.health > 0 && fighter2.health > 0) {
+            if(fighter1Turn) {
+                fighter2.health -= fighter1.damagePerAttack;
+            } else {
+                fighter1.health -= fighter2.damagePerAttack;
+            }
+            fighter1Turn = !fighter1Turn;
+        }
+        if(fighter1.health<=0)
+            return fighter2.name;
+        else
+            return fighter1.name;
+    }
+}
